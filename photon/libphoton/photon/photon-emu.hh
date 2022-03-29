7ec97decd36dd84fba529adce8d015d5b1e01e5f
manto128 <abhattacha24@wisc.edu>
2022-03-29 16:59:58 -0500

    Add upcycle ISA support

--- photon/libphoton/photon/photon-emu.hh
+++ photon/libphoton/photon/photon-emu.hh
@@ -35,25 +35,25 @@ struct UpcycleEmu {
     BINARY_OP(vadds_i8, int8_t,  64, arithmetic::adds_i8)
     BINARY_OP(vadd_u8,  uint8_t, 64, arithmetic::add_u8)
     BINARY_OP(vadds_u8, uint8_t, 64, arithmetic::adds_u8)
-    BINARY_OP(vadd_fp16, uint16_t, 32, arithmetic::add_fp16)
+    BINARY_OP(vadd_fp16, half, 32, arithmetic::add_fp16)
 
     BINARY_OP(vsub_i8,  int8_t,  64, arithmetic::sub_i8)
     BINARY_OP(vsubs_i8, int8_t,  64, arithmetic::subs_i8)
     BINARY_OP(vsub_u8,  uint8_t, 64, arithmetic::sub_u8)
     BINARY_OP(vsubs_u8, uint8_t, 64, arithmetic::subs_u8)
-    BINARY_OP(vsub_fp16, uint16_t, 32, arithmetic::sub_fp16)
+    BINARY_OP(vsub_fp16, half, 32, arithmetic::sub_fp16)
 
     BINARY_OP(vmul_i8,  int8_t,  64, arithmetic::mul_i8)
     BINARY_OP(vmuls_i8, int8_t,  64, arithmetic::muls_i8)
     BINARY_OP(vmul_u8,  uint8_t, 64, arithmetic::mul_u8)
     BINARY_OP(vmuls_u8, uint8_t, 64, arithmetic::muls_u8)
-    BINARY_OP(vmul_fp16, uint16_t, 32, arithmetic::mul_fp16)
+    BINARY_OP(vmul_fp16, half, 32, arithmetic::mul_fp16)
 
     BINARY_OP(vdiv_i8,  int8_t,  64, arithmetic::div_i8)
     BINARY_OP(vdivs_i8, int8_t,  64, arithmetic::divs_i8)
     BINARY_OP(vdiv_u8,  uint8_t, 64, arithmetic::div_u8)
     BINARY_OP(vdivs_u8, uint8_t, 64, arithmetic::divs_u8)
-    BINARY_OP(vdiv_fp16, uint16_t, 32, arithmetic::div_fp16)
+    BINARY_OP(vdiv_fp16, half, 32, arithmetic::div_fp16)
 
 #define FMA_8_32_OP(name, dt_acc, dt_inp, add, mul) \
     static inline void name(VREG_T& acc, VREG_T& src1, VREG_T& src2) { \
@@ -73,12 +73,31 @@ struct UpcycleEmu {
     FMA_8_32_OP(vfmas_u8_u32, uint32_t, uint8_t, arithmetic::adds_u32, arithmetic::mul_u8_u32)
 
     static inline void vfma_fp16(VREG_T& acc, VREG_T& src1, VREG_T& src2) {
-        uint16_t * src1_fp16 = (uint16_t *)src1.bytes;
-        uint16_t * src2_fp16 = (uint16_t *)src2.bytes;
-        uint16_t * acc_fp16 = (uint16_t *)acc.bytes;
+        half * src1_fp16 = (half *)src1.bytes;
+        half * src2_fp16 = (half *)src2.bytes;
+        half * acc_fp16 = (half *)acc.bytes;
         for (size_t i = 0; i < 32; i++)
             acc_fp16[i] += (src1_fp16[i] * src2_fp16[i]);
     }
+
+    #define UNARY_OP(name, dt, nelem, op) \
+    static inline void name(VREG_T& reg) { \
+        dt * reg_ ## dt = (dt *)reg.bytes; \
+        for (size_t i = 0; i < nelem; i++) \
+            (reg_ ## dt)[i] = op((reg_ ## dt)[i]); \
+    }
+
+    UNARY_OP(vrelu_i8,  int8_t,  64, arithmetic::relu_i8)
+    UNARY_OP(vrelu_u8,  uint8_t, 64, arithmetic::relu_u8)
+    UNARY_OP(vrelu_fp16, half, 32, arithmetic::relu_fp16)
+
+    UNARY_OP(vtanh_i8,  int8_t,  64, arithmetic::tanh_i8)
+    UNARY_OP(vtanh_u8,  uint8_t, 64, arithmetic::tanh_u8)
+    UNARY_OP(vtanh_fp16, half, 32, arithmetic::tanh_fp16)
+
+    UNARY_OP(vsigmoid_i8,  int8_t,  64, arithmetic::sigmoid_i8)
+    UNARY_OP(vsigmoid_u8,  uint8_t, 64, arithmetic::sigmoid_u8)
+    UNARY_OP(vsigmoid_fp16, half, 32, arithmetic::sigmoid_fp16)
 
     static inline void vld(uint8_t * addr, VREG_T& dst) {
         memcpy(dst.bytes, addr, 64);
