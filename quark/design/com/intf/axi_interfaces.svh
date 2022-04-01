754e5ac113558f35e09e6f2a0a2bbaf5bbb48b6b
Manjunath Madakasira <manjunath@snares-02.cs.wisc.edu>
2022-04-01 03:17:29 -0500

    adding read interfaces

--- quark/design/com/intf/axi_interfaces.svh
+++ quark/design/com/intf/axi_interfaces.svh
@@ -43,3 +43,68 @@ modport receiver (output axi_wready,
 
 endinterface:axi_wr_w_if
 
+interface axi_wr_b_if #(
+	parameter ID_WIDTH = 8
+) (input clk, input rst_n);
+
+logic [ID_WIDTH-1:0]    axi_bid;
+logic                   axi_bresp;
+logic                   axi_bvalid;
+logic                   axi_bready;
+
+modport sender (input axi_bready,
+		output axi_bid, axi_bresp, axi_bvalid);
+
+modport receiver (output axi_bready,
+		input axi_bid, axi_bresp, axi_bvalid);
+
+
+
+endinterface:axi_wr_b_if
+
+interface axi_rd_ar_if #(
+	parameter ID_WIDTH = 8,
+	parameter ADDR_WIDTH = 16
+) (input clk, input rst_n);
+
+logic [ID_WIDTH-1:0]    axi_arid;
+logic [ADDR_WIDTH-1:0]  axi_araddr;
+logic [7:0]             axi_arlen;
+logic [2:0]             axi_arsize;
+logic [1:0]             axi_arburst;
+logic                   axi_arlock;
+logic [3:0]             axi_arcache;
+logic [2:0]             axi_arprot;
+logic                   axi_arvalid;
+logic                   axi_arready;
+
+modport sender (input axi_arready,
+		output axi_arid, axi_araddr, axi_arlen, axi_arsize, axi_arburst, axi_arlock, axi_arcache, axi_arprot, axi_arvalid);
+
+
+modport receiver (output axi_arready,
+		input axi_arid, axi_araddr, axi_arlen, axi_arsize, axi_arburst, axi_arlock, axi_arcache, axi_arprot, axi_arvalid);
+
+endinterface:axi_rd_ar_if
+
+interface axi_rd_r_if #(
+	parameter ID_WIDTH = 8,
+	parameter DATA_WIDTH = 64
+) (input clk, input rst_n);
+
+logic [ID_WIDTH-1:0]    axi_rid;
+logic [DATA_WIDTH-1:0]  axi_rdata;
+logic [1:0]             axi_rresp;
+logic                   axi_rlast;
+logic                   axi_rvalid;
+logic                   axi_rready;
+
+modport sender (input axi_rready,
+		output axi_rid, axi_rdata, axi_rresp, axi_rlast, axi_rvalid);
+
+modport receiver (output axi_rready,
+		input axi_rid, axi_rdata, axi_rresp, axi_rlast, axi_rvalid);
+
+
+endinterface:axi_rd_r_if
+
--- quark/design/top/top_quark.sv
+++ quark/design/top/top_quark.sv
@@ -5,7 +5,10 @@ module top_quark (
 input clk,
 input rst_n,
 axi_wr_aw_if.receiver quark_wr_cmd_if,
-axi_wr_w_if.receiver quark_wr_data_if
+axi_wr_w_if.receiver quark_wr_data_if,
+axi_wr_b_if.sender quark_wr_bresp_if,
+axi_rd_ar_if.receiver quark_rd_cmd_if,
+axi_rd_r_if.sender quark_rd_data_if
 );
 
 endmodule
