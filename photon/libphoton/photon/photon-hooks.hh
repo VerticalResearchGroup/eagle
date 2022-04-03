2fa9202224c642026899412b05797947ca76e461
Michael Davies <michaelstoby@gmail.com>
2022-04-03 19:46:54 +0000

    Add hooks to emulator and rework structure

---
+++ photon/libphoton/photon/photon-hooks.hh
@@ -0,0 +1,25 @@
+#pragma once
+#include <vector>
+#include <functional>
+#include <stdint.h>
+
+namespace photon {
+
+template<class... Args>
+struct PhotonHook {
+    std::vector<std::function<void(Args...)>> callbacks;
+
+    void operator()(Args... args) {
+        for (auto& f : callbacks) {
+            f(args...);
+        }
+    }
+
+};
+
+struct PhotonHooks {
+    PhotonHook<size_t, uintptr_t> on_mem_read;
+    // TODO: Declare hooks here
+};
+
+}
