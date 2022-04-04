04a4d2089365122476a7ca86e393a934f5c15f51
Sagnik Basu <basusagnik1995@gmail.com>
2022-04-03 19:21:56 -0500

    Complete integration with electron

--- electron/electron-tf/test/relu.py
+++ electron/electron-tf/test/relu.py
@@ -4,8 +4,9 @@ import tensorflow as tf
 import numpy as np
 tf.compat.v1.disable_eager_execution()
 a = tf.random.normal(shape=[10], dtype=tf.float32)
-
+a = tf.random.uniform(shape=[10], minval=-1, maxval=5, dtype=tf.int32)
 with tf.device("/MY_DEVICE:0"):
+    #a = tf.constant([10, 12, 1, 0], dtype=tf.float32)
     b = tf.nn.relu(a)
 
 sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(allow_soft_placement=False, log_device_placement=True))
