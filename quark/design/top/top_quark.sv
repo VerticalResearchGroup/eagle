67812e889edbbe7d34b42d9236fb560a2aead2fe
Rutwik Jain <rnjain@helin.cs.wisc.edu>
2022-04-05 14:52:54 -0500

    Added Core<->L1Cache Interfaces

--- quark/design/top/top_quark.sv
+++ quark/design/top/top_quark.sv
@@ -1,5 +1,6 @@
 
 `include "../com/intf/axi_interfaces.svh"
+`include "../com/intf/cache_interfaces.svh"
 
 module top_quark (
 input clk,
@@ -8,7 +9,7 @@ axi_wr_aw_if.receiver quark_wr_cmd_if,
 axi_wr_w_if.receiver quark_wr_data_if,
 axi_wr_b_if.sender quark_wr_bresp_if,
 axi_rd_ar_if.receiver quark_rd_cmd_if,
-axi_rd_r_if.sender quark_rd_data_if
+axi_rd_r_if.sender quark_rd_data_if,
 );
 
 endmodule
