1ae5542cac9cc7e575e61520991edb6a968a58b8
Rutwik Jain <rnjain@helin.cs.wisc.edu>
2022-04-05 15:05:37 -0500

    top_quark testing

--- quark/design/top/top_quark.sv
+++ quark/design/top/top_quark.sv
@@ -9,7 +9,7 @@ axi_wr_aw_if.receiver quark_wr_cmd_if,
 axi_wr_w_if.receiver quark_wr_data_if,
 axi_wr_b_if.sender quark_wr_bresp_if,
 axi_rd_ar_if.receiver quark_rd_cmd_if,
-axi_rd_r_if.sender quark_rd_data_if,
+axi_rd_r_if.sender quark_rd_data_if
 );
 
 endmodule
