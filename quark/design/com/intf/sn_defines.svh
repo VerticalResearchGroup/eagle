11948174c15579683684270ef33c8f2a3b715dc7
Steve Landherr <landherr@cs.wisc.edu>
2022-04-18 17:47:22 -0500

    Interfaces and Operation defines for the signaling network

---
+++ quark/design/com/intf/sn_defines.svh
@@ -0,0 +1,4 @@
+// Tile Operations
+`define OP_NOP       0
+`define OP_DISPATCH  1
+`define OP_COMPLETED 2
---
+++ quark/design/com/intf/sn_interfaces.svh
@@ -0,0 +1,24 @@
+// Signaling Network Related Interfaces
+
+/*   Signaling Ring Messages
+*    Used by GC FSM to send messages and by tiles to receive them
+*/
+interface sn_ring_msg #(
+  parameter TILE_WIDTH = 4,
+  parameter ADDR_WIDTH = 64,
+  parameter WL_LEN_BITS = 32 
+) (input clk, input rst_n);
+
+logic                     op;
+logic [TILE_WIDTH-1:0]    tile;
+logic [ADDR_WIDTH-1:0]    addr;
+logic [WL_LEN_BITS-1:0]   len;
+logic                     done;
+
+modport sender (input done,
+  output op, tile, addr, len);
+
+modport receiver(output done,
+  input op, tile, addr, len);
+
+endinterface:sn_ring_msg
